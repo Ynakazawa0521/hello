8e50f10e04bf7991c1ccdb46f5c22a115e82c352
Ynakazawa0521 <yuki.nakazawa@mikilab.doshisha.ac.jp>
2025-05-21 16:20:10 +0900

    Upate hello2.py

--- hello2.py
+++ hello2.py
@@ -1,1 +1,9 @@
-print("はろう")
+from datetime import datetime
+
+now = datetime.now()
+print("現在の時刻は:", now.strftime("%H:%M:%S"))
+
+if now.hour < 12:
+    print("おはようございます！")
+else:
+    print("こんにちは！")
